f0d8d3849a9bc303ed89f7ec5686b7d31c3e35b8
Miguel <miguelmds925@gmail.com>
2023-07-07 17:03:36 -0300

    Adicionando alguns eventos ao projeto

--- tier-list/script.js
+++ tier-list/script.js
@@ -9,22 +9,30 @@ document.querySelectorAll(".span-number").forEach(span => {
   span.addEventListener("drop", drop)
 })
 
-function dragstart() {
-
+function dragStart(event) {
+  event.currentTarget.classList.add("dragging");
 }
 
-function dragEnd() {
-
+function dragEnd(event) {
+  e.currentTarget.classList.remove("dragging")
 }
 
-function dragOver() {
-
+function dragOver(event) {
+  if(event.currentTarget.querySelector(".span-number") === null){
+    event.preventDefault()
+    event.currentTarget.classList.add("hover")
+  }
 }
 
-function dragLeave() {
-
+function dragLeave(event) {
+  event.currentTarget.classList.remove("hover")
 }
 
-function drop() {
-  
+function drop(event) {
+  event.currentTarget.classList.remove("hover")
+
+  if(e.currentTarget.querySelector(".span-number") === null) {
+    let dragItem = document.querySelector(".span-number.dragging")
+    e.currentTarget.appendChild(dragItem)
+  }
 }
