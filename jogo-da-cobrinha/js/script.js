43876ba3b51b62bfec149a06a7cc6cd266e1f671
Miguel <miguelmds925@gmail.com>
2023-08-02 16:23:10 -0300

    Fazendo a cobra se mover

--- jogo-da-cobrinha/js/script.js
+++ jogo-da-cobrinha/js/script.js
@@ -5,16 +5,56 @@ const size = 30
 
 const snake = [
   {x: 200, y: 200},
-  {x: 230, y: 200},
-  {x: 260, y: 200}
+  {x: 230, y: 200}
 ]
+
+let direction , loopId
 
 const drawSnake = () => {
   ctx.fillStyle = "#ddd"
   
-  snake.forEach((position)=> {
+  snake.forEach((position, index)=> {
+
+    if(index == snake.length - 1) {
+      ctx.fillStyle = "white"
+    }
     ctx.fillRect(position.x, position.y, size, size)
   })
 }
 
-drawSnake()
+const moveSnake = () => {
+  if(!direction) return
+
+  const head = snake[snake.length - 1]
+
+  if(direction == "right") {
+    snake.push({x: head.x + size , y: head.y})
+  }
+
+  if(direction == "left") {
+    snake.push({x: head.x - size , y: head.y})
+  }
+
+  if(direction == "down") {
+    snake.push({x: head.x , y: head.y + size})
+  }
+
+  if(direction == "up") {
+    snake.push({x: head.x , y: head.y - size})
+  }
+  snake.shift()
+}
+
+const gameLoop = () => {
+  clearInterval(loopId)
+  ctx.clearRect(0, 0, 600, 600)
+
+  drawSnake()
+  moveSnake()
+
+   loopId = setTimeout(()=> {
+    gameLoop()
+  }, 300)
+}
+
+gameLoop()
