6ae628557c963222b124fd58fd0549a5b5186a8e
Miguel <miguelmds925@gmail.com>
2023-08-08 18:37:24 -0300

    Fazendo a comida do jogo da cobra em posições aleatórias e cores aleatórias

--- jogo-da-cobrinha/js/script.js
+++ jogo-da-cobrinha/js/script.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
+const h1 = document.querySelector("h1")
 
 const size = 30
 
@@ -7,7 +8,43 @@ const snake = [
   {x: 270, y: 270},
 ]
 
+const randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+  const number = randomNumber(0, canvas.width - size)
+  return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+  const red = randomNumber(0, 255)
+  const green = randomNumber(0, 255)
+  const blue = randomNumber(0, 255)
+
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+h1.innerText = randomColor()
+
+const food = {
+  x: randomPosition(),
+  y: randomPosition(),
+  color: randomColor()
+}
+
 let direction , loopId
+
+const drawFood = () => {
+
+  const{x, y, color} = food
+
+  ctx.shadowColor = color
+  ctx.shadowBlur = 50
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, size, size)
+  ctx.shadowBlur = 0
+}
 
 const drawSnake = () => {
   ctx.fillStyle = "#ddd"
@@ -72,6 +109,7 @@ const gameLoop = () => {
   ctx.clearRect(0, 0, 600, 600)
 
   drawGrid()
+  drawFood()
   drawSnake()
   moveSnake()
 
