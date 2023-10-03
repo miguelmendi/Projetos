dac8205777ef220bfbaa37f9db74b9a9b994cb80
Miguel <miguelmds925@gmail.com>
2023-10-03 19:06:51 -0300

    Pegando os dados do pokemon

---
+++ Pokedex/js/script.js
@@ -0,0 +1,8 @@
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+  const data = await APIResponse.json()
+  return data
+
+  
+}
