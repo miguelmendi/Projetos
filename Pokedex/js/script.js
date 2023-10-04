8c16c6122bc3d42a162f098811493e5daae143b7
Miguel <miguelmds925@gmail.com>
2023-10-04 16:34:21 -0300

    Pegando os dados do pekemon e fazendo aparecer na tela

--- Pokedex/js/script.js
+++ Pokedex/js/script.js
@@ -1,3 +1,7 @@
+const pokemonName = document.querySelector(".pokemon__name")
+const pokemonNumber = document.querySelector(".pokemon__number")
+const pokemonImage = document.querySelector(".pokemon__image")
+
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
@@ -6,3 +10,13 @@ const fetchPokemon = async (pokemon) => {
 
   
 }
+
+const renderPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon)
+
+  pokemonName.innerHTML = data.name
+  pokemonNumber.innerHTML = data.id
+  pokemonImage.src = data["sprites"]['versions']['generation-v']["black-white"]["animated"]["front_default"]
+}
+
+renderPokemon("225")
