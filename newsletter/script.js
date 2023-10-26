aea8d49e73f1d520d1ec176f82e181d94f6e74e3
Miguel <miguelmds925@gmail.com>
2023-10-26 20:26:05 -0300

    Pegando o valor do input e colocando na mensagem

--- newsletter/script.js
+++ newsletter/script.js
@@ -1,5 +1,6 @@
 let input = document.querySelector(".input")
 let close = document.querySelector(".close")
+const email =document.querySelector("#email")
 
 close.addEventListener("click", () => {
   aside.classList.remove("active")
@@ -12,9 +13,16 @@ function validateEmail(email) {
 
 let button = document.querySelector('#button');
 let aside = document.querySelector(".aside")
+const dismiss = document.querySelector("#dismiss")
+
+dismiss.addEventListener("click", () => {
+  aside.classList.add("active")
+})
 
 button.addEventListener("click", () => {
   aside.classList.add("active")
+  inputValue = input.value
+  email.innerHTML = inputValue
 })
 
 
