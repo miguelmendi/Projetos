f5c41a1e93c31cb0f2392a86817258489089dfaa
Miguel <miguelmds925@gmail.com>
2023-12-19 18:14:26 -0300

    Fazendo a função que termina o jogo

--- Memory-game/js/game.js
+++ Memory-game/js/game.js
@@ -22,6 +22,14 @@ const createElement = (tag, className) => {
 let firstCard = ""
 let secondCard = ""
 
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll(".disabled-card")
+
+  if(disabledCards.length === 20) {
+    alert("Prabéns, você conseguiu")
+  }
+}
+
 const checkCards = () => {
   const firstCharacter = firstCard.getAttribute("data-character")
   const SecondCharacter = secondCard.getAttribute("data-character")
@@ -32,6 +40,8 @@ const checkCards = () => {
     
     firstCard = ""
     secondCard = ""
+
+    checkEndGame()
   } else {
     setTimeout (() => {
       firstCard.classList.remove("reveal-card")
