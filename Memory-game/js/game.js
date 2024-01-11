6f35f2216fec214e81559092f0e32eff8f43ab0c
Miguel <miguelmds925@gmail.com>
2024-01-11 16:40:40 -0300

    Adicionando um timer ao jogo

--- Memory-game/js/game.js
+++ Memory-game/js/game.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector(".grid")
 const spanPlayer = document.querySelector(".player")
+const timer = document.querySelector(".timer")
 
 const characters = [
   "beth",
@@ -27,7 +28,8 @@ const checkEndGame = () => {
   const disabledCards = document.querySelectorAll(".disabled-card")
 
   if(disabledCards.length === 20) {
-    alert("Prabéns, você conseguiu")
+    clearInterval(this.loop)
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML} segundos`)
   }
 }
 
@@ -104,9 +106,17 @@ const LoadGame = () => {
   })
 }
 
+const startTimer = () => {
+  this.loop = setInterval(()=> {
+    const currentTime = +timer.innerHTML
+    timer.innerHTML = currentTime + 1
+  }, 1000)
+}
+
 window.onload = () => {
   spanPlayer.innerHTML = localStorage.getItem("player")
 
+  startTimer()
   LoadGame()
 }
 
